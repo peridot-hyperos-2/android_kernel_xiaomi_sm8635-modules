c6e1a267b3cec0b85ad2c14c521c886361dd0e92
Jens Reidel <adrian@travitia.xyz>
2025-12-04 23:01:23 +0545

    display-drivers: mi_disp: Only enable debugfs if supported

--- qcom/opensource/display-drivers/msm/mi_disp/mi_sde_connector.c
+++ qcom/opensource/display-drivers/msm/mi_disp/mi_sde_connector.c
@@ -147,6 +147,7 @@ int mi_sde_connector_register_esd_irq(struct sde_connector *c_conn) {
   return rc;
 }
 
+#if MI_DISP_DEBUGFS_ENABLE
 int mi_sde_connector_debugfs_esd_sw_trigger(void *display) {
   struct dsi_display *dsi_display = (struct dsi_display *)display;
   struct drm_connector *connector = NULL;
@@ -210,6 +211,7 @@ int mi_sde_connector_debugfs_esd_sw_trigger(void *display) {
 
   return 0;
 }
+#endif
 
 int mi_sde_connector_state_get_mi_mode_info(
     struct drm_connector_state *conn_state, struct mi_mode_info *mode_info) {
