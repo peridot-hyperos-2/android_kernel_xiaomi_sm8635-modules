4241fd001e1a2118b559af765337f2b81f5234c8
Pzqqt <821026875@qq.com>
2025-12-04 22:05:54 +0545

    asoc: codecs: aw882xx: Enable fade in/out by default

--- qcom/opensource/audio-kernel/asoc/codecs/pa_combine/src/aw882xx/aw882xx_device.c
+++ qcom/opensource/audio-kernel/asoc/codecs/pa_combine/src/aw882xx/aw882xx_device.c
@@ -1073,11 +1073,11 @@ static int aw_device_parse_sound_channel_dt(struct aw_device *aw_dev) {
 
 void aw_device_parse_fade_flag_dt(struct aw_device *aw_dev) {
   int ret;
-  uint32_t fade_en = 0;
+  uint32_t fade_en = 1;
 
   ret = of_property_read_u32(aw_dev->dev->of_node, "fade-flag", &fade_en);
   if (ret < 0)
-    aw_dev_info(aw_dev->dev, "read fade-flag failed,use default");
+    aw_dev_info(aw_dev->dev, "read fade-flag failed, default enable");
 
   aw_dev->fade_en = fade_en;
   aw_dev_info(aw_dev->dev, "fade-flag: %d", fade_en);
